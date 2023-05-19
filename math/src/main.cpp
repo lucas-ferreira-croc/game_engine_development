2c10c1b525e2c2c454fc6f9696951ba0fbd76df3
unknown <lucas.ferreira057@bandtec.com.br>
2023-05-19 01:25:25 -0300

    vector multiplication, division, magnitude and normalization

--- math/src/main.cpp
+++ math/src/main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
+#include "vectors/vector3D.h"
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    vector3D vec = vector3D(1.0f, 2.0f, 3.0f);
+    vec.print();
+
+    /*
+        vec[0] = 6;
+        vec[1] = 5;
+        vec[2] = 7;
+    */
+
+    vec = vec * 2; 
+    vec.Normalize();
+    vec.print();
+    std::cout << vec.Magnitude() << std::endl;
+    vec /= 2;
+    vec.print();
+    std::cout << vec.Magnitude() << std::endl;
+    vec = vec / 2;
+    vec.print();
+    std::cout << vec.Magnitude() << std::endl;
+    vec = -vec;
+    vec.print();
+    std::cout << vec.Magnitude() << std::endl;
 }
--- math/src/vectors/vector3D.h
+++ math/src/vectors/vector3D.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 class vector3D
 {
@@ -24,6 +25,61 @@ public:
     const float& operator [] (int i) const
     {
         return ((&x)[i]);
+    }
+
+    vector3D& operator *= (float s)
+    {
+        this->x *= s;
+        this->y *= s;
+        this->z *= s;
+
+        return *this;
+    }
+
+    vector3D& operator * (float s)
+    {
+        this->x *= s;
+        this->y *= s;
+        this->z *= s;
+        return *this;
+    }
+
+    vector3D& operator /= (float s)
+    {
+        s = 1.0f / s;
+        this->x *= s;
+        this->y *= s;
+        this->z *= s;
+
+        return *this;
+    }    
+
+    vector3D& operator / (float s)
+    {
+        s = 1.0f / s;
+        this->x *= s;
+        this->y *= s;
+        this->z *= s;
+
+        return *this;
+    }
+
+    vector3D& operator -()
+    {
+        this->x = -this->x;
+        this->y = -this->y;
+        this->z = -this->z;
+        return *this;
+    }
+
+    float Magnitude()
+    {
+        return sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
+    }
+
+    vector3D& Normalize()
+    {
+        return (*this / Magnitude());
     }
 
     void print()
